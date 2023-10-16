c1b0b48cb543dd6db3e0f7095e0eb243aaa9c89e
nextples <gankeev@gmail.com>
2023-10-17 00:40:34 +0700

    add .resize, .clear, .push_back

--- task-1/BitArray.cpp
+++ task-1/BitArray.cpp
@@ -1,4 +1,5 @@
 #include "BitArray.h"
+#include <limits.h>
 
 #define BITS_IN_LONG sizeof(unsigned long)*8
 
@@ -11,6 +12,14 @@ unsigned long CheckBit(const int value, const int position) {
         result = 1;
     }
     return result;
+}
+
+unsigned long SetZeroToBit(unsigned long value, int position) {
+    return (value & ~(1 << position));
+}
+
+unsigned long SetOneToBit(unsigned long value, int position) {
+    return (value | (1 << position));
 }
 
 BitArray::BitArray() {      // create array constructor
@@ -70,7 +79,6 @@ BitArray & BitArray::operator = (const BitArray &other) {
 }
 
 void BitArray::resize(int num_bits, bool value) {   //Изменяет размер массива. В случае расширения, новые элементы инициализируются значением value.
-    bitSize = num_bits;
     int newElSize = 0;
     if (num_bits % sizeof(unsigned long) == 0) {
         newElSize = (int)(num_bits / sizeof(unsigned long));
@@ -79,11 +87,67 @@ void BitArray::resize(int num_bits, bool value) {   //Изменяет размер массива. В
         newElSize = (int)(num_bits / sizeof(unsigned long) + 1);
     }
     unsigned long *newArray = new unsigned long [newElSize];
-    if (newElSize <= )
-    for (int i = 0; i < newElSize; i++) {
-        newArray[i] = array[i];
+    if (newElSize <= elSize) {
+        for (int i = 0; i < newElSize; i++) {
+            newArray[i] = array[i];
+        }
     }
-
-
-
+    else {
+        for (int i = 0; i < elSize; i++) { //   copy the previous version
+            newArray[i] = array[i];
+        }
+        for (int i = BITS_IN_LONG - (bitSize % BITS_IN_LONG) - 1; i >= 0; i--) {
+            if (value) {
+                newArray[elSize - 1] = SetOneToBit(newArray[elSize - 1], i);
+            }
+            else {
+                newArray[elSize - 1] = SetZeroToBit(newArray[elSize - 1], i);
+            }
+        }
+        for (int i = elSize; i < newElSize; i++) {
+            if (value) {
+                newArray[i] = LONG_MAX;
+            }
+            else {
+                newArray[i] = 0;
+            }
+        }
+    }
+    delete[] array;
+    array = newArray;
+    elSize = newElSize;
+    bitSize = num_bits;
 }
+
+void BitArray::clear() {
+    delete[] array;
+    bitSize = 0;
+    elSize = 0;
+}
+
+void BitArray::push_back(bool bit) {
+    if (elSize * BITS_IN_LONG - bitSize > 0) {      //   memory doesn't need to be added
+        if (bit) {
+            array[elSize - 1] = SetOneToBit(array[elSize - 1], BITS_IN_LONG - (bitSize % (int)BITS_IN_LONG) - 1);
+            bitSize++;
+        }
+        else {
+            array[elSize - 1] = SetZeroToBit(array[elSize - 1], BITS_IN_LONG - (bitSize % (int)BITS_IN_LONG) - 1);
+            bitSize++;
+        }
+    }
+    else {      //   memory need to be added
+        resize(bitSize + 1);
+        if (bit) {
+            array[elSize] = SetOneToBit(array[elSize], BITS_IN_LONG - (bitSize % (int)BITS_IN_LONG) - 1);
+            elSize++;
+            bitSize++;
+        }
+        else {
+            array[elSize] = SetZeroToBit(array[elSize], BITS_IN_LONG - (bitSize % (int)BITS_IN_LONG) - 1);
+            elSize++;
+            bitSize++;
+        }
+    }
+}
+
--- task-1/main.cpp
+++ task-1/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
-#include "BitArray.h"
+
+
+unsigned long SetZeroToBit(unsigned long value, int position) {
+    return (value & ~(1 << position));
+}
+
+unsigned long SetOneToBit(unsigned long value, int position) {
+    return (value | (1 << position));
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+//    std::cout << sizeof(unsigned long) << std::endl;
+    unsigned long val = 1;
+    val = SetOneToBit(val, 0);
+    std::cout << val << std::endl;
+    val = SetZeroToBit(val, 0);
+    std::cout << val << std::endl;
+    val = SetOneToBit(val, 1);
+    std::cout << val << std::endl;
+    val = SetZeroToBit(val, 1);
+    std::cout << val << std::endl;
+    val = SetOneToBit(val, 2);
+    std::cout << val << std::endl;
+    val = SetZeroToBit(val, 2);
+    std::cout << val << std::endl;
+
     return 0;
-    BitArray array;
 }
